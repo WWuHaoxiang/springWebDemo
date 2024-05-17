551f01067fdd9e105074e442394e7930ddf46be4
butter_eggroll <m0_61499126@gitcode.net>
2024-05-17 13:27:16 +0800

    “zzxsa"

---
+++ src/main/java/com/e4_student/E4StudentApplication.java
@@ -0,0 +1,13 @@
+package com.e4_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E4StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E4StudentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/e4_student/E4StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e4_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E4StudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
